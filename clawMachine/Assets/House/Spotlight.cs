2fd5fcba1cde2b1ea7ff3dd457317b5b06b2e1a9
Houshikan Yokeswaran <hau5tastic@gmail.com>
2018-11-01 13:51:23 -0400

    master push

---
+++ clawMachine/Assets/House/Spotlight.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spotlight : MonoBehaviour
+{
+    public float colorChangeDuration = 1f;
+    private float elapsed;
+    private Light spotlight;
+    public bool changeColors = false;
+    private bool changeToColor = false;
+    private Color from;
+    private Color to;
+
+    private void Awake()
+    {
+        spotlight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        if (changeColors)
+        {
+            LerpLightColor();
+            if (elapsed > colorChangeDuration)
+            {
+                elapsed = 0f;
+                SetLerpColors();
+            }
+        }
+    }
+
+    public void ChangeColors()
+    {
+        changeColors = true;
+    }
+
+    public void LerpToColor(Color color)
+    {
+        from = spotlight.color;
+        to = color;
+        changeColors = true;
+    }
+
+    private void SetLerpColors()
+    {
+        from = spotlight.color;
+        to = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+    }
+
+    private void LerpLightColor()
+    {
+        elapsed += Time.deltaTime;
+        spotlight.color = Color.Lerp(from, to, elapsed / colorChangeDuration);
+    }
+}
---
+++ clawMachine/Assets/House/SpotlightManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpotlightManager : MonoBehaviour
+{
+    public Color EnablePink;
+
+    [Space]
+    [SerializeField] private Animator animator;
+    [SerializeField] Light mainSpotLight;
+    [SerializeField] Spotlight interiorLight;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            MainSpotLight();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            EnableInteriorLight();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            EnableStrobeInteriorLight(true, 1f);
+        }
+    }
+
+    public void MainSpotLight()
+    {
+        animator.SetTrigger("mainSpotlight");
+    }
+
+    public void EnableInteriorLight()
+    {
+        interiorLight.gameObject.SetActive(true);
+    }
+
+    public void EnableStrobeInteriorLight(bool isOn, float f)
+    {
+        interiorLight.changeColors = isOn;
+        interiorLight.colorChangeDuration = f;
+    }
+}
